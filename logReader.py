6323a8888d426f551be2e4018617be46a3e096b8
Keita Suzuki <keitasuzuki.park@sslab.ics.keio.ac.jp>
2019-06-20 17:24:37 +0900

    init commit

---
+++ logReader.py
@@ -0,0 +1,17 @@
+import neovim
+import pynvim
+
+@pynvim.plugin
+class Main(object):
+    def __init__(self, vim):
+        self.vim = vim
+
+    @pynvim.function('readSTFreeLog', nargs="1")
+    def doReadSTFreeLog(self, args):
+        lines = []
+        with open(args[0], mode="r") as f:
+            lines = f.readlines()
+        warnings = ''.join(lines)
+        self.vim.command('set "errorformat+=[%#] %f:%l:%c %m"')
+        self.vim.command('cexpr "{}"'.format(warnings))
+        return
