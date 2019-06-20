5d1cf7e260e05852b5105bd1cf53b27cbe076f5e
Keita Suzuki <keitasuzuki.park@sslab.ics.keio.ac.jp>
2019-06-20 09:43:11 +0000

    Update to movable

--- rplugin/python3/logReader.py
+++ rplugin/python3/logReader.py
@@ -6,12 +6,12 @@ class Main(object):
     def __init__(self, vim):
         self.vim = vim
 
-    @pynvim.command('ReadLog')
+    @pynvim.command('ReadLog', nargs='*')
     def doReadSTFreeLog(self, args):
         lines = []
+        self.vim.command('set errorformat+="[%#] %f:%l:%c %m"')
         with open(args[0], mode="r") as f:
             lines = f.readlines()
         warnings = ''.join(lines)
-        self.vim.command('set errorformat+="[%#] %f:%l:%c %m"')
-        self.vim.command('cexpr "{}"'.format(warnings))
+        self.vim.command('cexpr system("cat {}")'.format(args[0]))
         return
