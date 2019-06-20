4847d1c9933ad646431c234fdd7dd0e29930da6b
Keita Suzuki <keitasuzuki.park@sslab.ics.keio.ac.jp>
2019-06-20 18:09:05 +0900

    Minor updates

--- rplugin/python3/logReader.py
+++ rplugin/python3/logReader.py
@@ -6,12 +6,12 @@ class Main(object):
     def __init__(self, vim):
         self.vim = vim
 
-    @pynvim.function('readSTFreeLog', nargs="1")
+    @pynvim.function('readSTFreeLog')
     def doReadSTFreeLog(self, args):
         lines = []
         with open(args[0], mode="r") as f:
             lines = f.readlines()
         warnings = ''.join(lines)
-        self.vim.command('set "errorformat+=[%#] %f:%l:%c %m"')
+        self.vim.command('set errorformat+="[%#] %f:%l:%c %m"')
         self.vim.command('cexpr "{}"'.format(warnings))
         return
